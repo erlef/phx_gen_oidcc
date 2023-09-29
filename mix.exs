881a061590ecb2d3b5fa0f8eae879c1b5173569c
Jonatan Männchen <jonatan@maennchen.ch>
2023-09-30 01:05:35 +0200

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxGenOidcc.MixProject do
   def project do
     [
       app: :phx_gen_oidcc,
-      version: "0.1.0-rc.2",
+      version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
